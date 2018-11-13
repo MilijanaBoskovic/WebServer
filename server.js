cea8de3a4a4d0355cfdfcf52857a5bd45f0ae7b0
unknown <alejna94@gmail.com>
2018-11-13 09:20:34 +0100

    server modified

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const path=require('path');
+
 const port=process.env.PORT || 3000;
 var app=express();
 
-hbs.registerPartials(__dirname + '\\views\\partials');
+hhbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 app.set('view engine','hbs');
-app.use(express.static(__dirname+ '\\public'));
+app.use(express.static(path.join(__dirname, 'public'));
 
 app.use((req,res,next)=>{
   var now=new Date().toString();
